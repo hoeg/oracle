66abef8008041bc9831e007d3aa62ac7553f954e
Peter Hoeg Steffensen <peter.steffensen@gmail.com>
2023-12-22 13:58:01 +0100

    fix variable

---
+++ ask.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Error: First argument is missing."
+  exit 1
+fi
+
+if ! [[ "$1" =~ ^[a-zA-Z]+$ ]]; then
+  echo "Error: First argument is not a valid string."
+  exit 1
+fi
+
+REPO=$1
+
+semgrep scan --config questions/ $REPO
